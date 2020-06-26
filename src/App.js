5684bbdb46436a5e4c972fe6388f611c30579427
Minakshi Verma <mvedwan@gmail.com>
2020-06-26 00:13:43 -0400

    reconstructing the app to fix deployment problem

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cellular from './Cellular'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Cellular />
+    </div>
+  );
+}
+
+export default App;
